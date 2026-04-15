bbd7c4aeb981bc55d5f3c211445bfe71e6b23ef7
Igorkruf <igor.kruf@gmail.com>
2026-04-15 21:37:47 +0300

    внесли информацию в README.md что касается переменных окружения

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from '#routes/user.js';
+import DotenvFlow from 'dotenv-flow';
+DotenvFlow.config() 
 
 let app = express();
-
+console.log(process.env.DATABASE_PORT)
 app.use('/user/', userRouter)
 app.get('/', function(req, res) {
 	res.send('hello world');
